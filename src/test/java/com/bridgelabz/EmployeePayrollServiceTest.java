9d8b0ee7157413867a96d7ea937e140179c5f1e5
Shubham <ssp671995@gmail.com>
2022-04-19 01:06:00 +0530

    Added : Find Salary By Gender

--- src/test/java/com/bridgelabz/EmployeePayrollServiceTest.java
+++ src/test/java/com/bridgelabz/EmployeePayrollServiceTest.java
@@ -69,8 +69,22 @@ public class EmployeePayrollServiceTest {
         EmployeePayrollService employeePayrollService = new EmployeePayrollService();
         List<EmployeePayrollData> employeePayrollData = employeePayrollService.readData(EmployeePayrollService.IOService.DB_IO);
         List<EmployeePayrollData> employeeDataInGivenDateRange = employeePayrollService
-                .getEmployeesInDateRange("2018-01-01", "2019-11-15");
+                .getEmployeesInDateRange("2019-01-14", "2020-05-05");
         assertEquals(2, employeeDataInGivenDateRange.size());
         System.out.println(employeePayrollData);
+    }
+
+    // UC6
+    @Test
+    /**
+     * to test When Average Salary Retrieved By Gender Should Return Proper Value
+     */
+    public void givenPayrollData_WhenAverageSalaryRetrievedByGender_ShouldReturnProperValue() {
+        EmployeePayrollService employeePayrollService = new EmployeePayrollService();
+        employeePayrollService.readData(EmployeePayrollService.IOService.DB_IO);
+        Map<String, Double> averageSalaryByGender = employeePayrollService.readAverageSalaryByGender(EmployeePayrollService.IOService.DB_IO);
+        System.out.println(averageSalaryByGender);
+        assertTrue(
+                averageSalaryByGender.get("M").equals(200000.0) && averageSalaryByGender.get("F").equals(3000000.0));
     }
 }
