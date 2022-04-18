c0ef7dba4b7e272dfa096ae43e6d9a4b098bfdaf
Shubham <ssp671995@gmail.com>
2022-04-19 01:04:27 +0530

    Added : Retrive data from given data range

--- src/test/java/com/bridgelabz/EmployeePayrollServiceTest.java
+++ src/test/java/com/bridgelabz/EmployeePayrollServiceTest.java
@@ -57,4 +57,20 @@ public class EmployeePayrollServiceTest {
         assertTrue(result);
         System.out.println(employeePayrollData);
     }
+    @Test
+    /**
+     * To test whether the count of the retrieved data who have joined in a
+     * particular data range matches with the expected value
+     *
+     * @throws EmployeePayrollException
+     */
+    public void givenDateRangeForEmployee_WhenRetrievedUsingStatement_ShouldReturnProperData()
+            throws EmployeePayrollException {
+        EmployeePayrollService employeePayrollService = new EmployeePayrollService();
+        List<EmployeePayrollData> employeePayrollData = employeePayrollService.readData(EmployeePayrollService.IOService.DB_IO);
+        List<EmployeePayrollData> employeeDataInGivenDateRange = employeePayrollService
+                .getEmployeesInDateRange("2018-01-01", "2019-11-15");
+        assertEquals(2, employeeDataInGivenDateRange.size());
+        System.out.println(employeePayrollData);
+    }
 }
