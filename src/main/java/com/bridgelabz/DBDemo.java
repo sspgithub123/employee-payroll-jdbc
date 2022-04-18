8ba16e19fd196f8e5361cb280005c6c712d91beb
Shubham <ssp671995@gmail.com>
2022-04-19 00:51:48 +0530

    Added the welcome message in the program.

---
+++ src/main/java/com/bridgelabz/DBDemo.java
@@ -0,0 +1,19 @@
+package com.bridgelabz;
+
+/**
+ * @author : Shubham Pawar
+ * @Since :18-04-2022
+ * @purpose : MySQL DB
+ */
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.util.Enumeration;
+
+public class DBDemo {
+    public static void main(String[] args) {
+        System.out.println("welcome to employee-payroll mysql");
+
+    }
+}
